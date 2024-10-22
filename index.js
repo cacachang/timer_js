93b188d5ff53495a33acfe0e12a2d8bb9fe85a2d
Ning <a24701770@gmail.com>
2024-10-22 13:27:24 +0800

    add: timer calculator

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function startTimer () {
+  let timeInput = document.querySelector("#timeInput").value
+  let display = document.querySelector("#display")
 
+
+  let timer = setInterval(() => {
+    if (timeInput > 0) {
+      display.innerHTML = `剩餘時間: ${timeInput} 秒`;
+      timeInput--;
+    } else {
+      clearInterval(timer)
+      display.innerHTML = '時間到！';
+    }
+  }, "1000");
 }
